196b330f21da517ea42e93167f918debfb00c697
Edward Thomas <90358655+edwardfgt@users.noreply.github.com>
2024-09-17 16:04:44 +0100

    Setup linting and prettier

---
+++ .eslintrc.js
@@ -0,0 +1,12 @@
+module.exports = {
+  extends: [
+    'eslint:recommended',
+    'plugin:react/recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:react-hooks/recommended',
+    'prettier'
+  ],
+  parser: '@typescript-eslint/parser',
+  plugins: ['react', '@typescript-eslint'],
+  root: true,
+};
