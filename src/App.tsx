92c2969042bcf37dd5aacfc0b267ff650b466450
Edward Thomas <90358655+edwardfgt@users.noreply.github.com>
2024-09-18 23:43:18 +0100

    Create basic navbar component

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,11 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from './components/Homepage';
 import Contact from './components/ContactForm';
-
+import Navbar from './components/Navbar';
 function App() {
   return (
     <Router>
+      <Navbar />
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/contact" element={<Contact />} />
