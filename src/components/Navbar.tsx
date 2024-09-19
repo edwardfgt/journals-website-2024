fea46aef8f7f2cb5239b3de5d111022c2dabce90
Edward Thomas <90358655+edwardfgt@users.noreply.github.com>
2024-09-19 16:50:40 +0100

    Clean up spacing in the navbar

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -4,10 +4,9 @@ import { Link } from 'react-router-dom';
 const Navbar: React.FC = () => {
     return (
         <nav className="bg-gray-800 p-4">
-            <div className="container mx-auto flex justify-between items-center">
-                <Link to="/" className="text-white font-bold text-xl">Journals.gg</Link>
+            <div className="container mx-auto flex items-center">
+                <Link to="/" className="text-white font-bold text-xl pr-8">Journals.gg</Link>
                 <div className="space-x-4">
-                    <Link to="/" className="text-white hover:text-gray-300">Home</Link>
                     <Link to="/contact" className="text-white hover:text-gray-300">Contact</Link>
                 </div>
             </div>
