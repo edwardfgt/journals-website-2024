7578042e798f19ab7ff799448484199b008c1b38
Edward Thomas <90358655+edwardfgt@users.noreply.github.com>
2024-09-20 15:48:12 +0100

    Change colours on contact page

--- src/components/ContactForm.tsx
+++ src/components/ContactForm.tsx
@@ -34,19 +34,19 @@ function ContactForm() {
     };
 
     return (
-        <section className="min-h-screen bg-gray-900 flex flex-col items-center justify-center">
+        <section className="min-h-screen bg-background flex flex-col items-center justify-center">
             <div className="px-4 mx-auto max-w-2xl">
-                <h2 className="mb-4 text-4xl tracking-tight font-extrabold text-center text-white">
+                <h2 className="mb-4 text-4xl tracking-tight font-extrabold text-center text-text">
                     Say hi
                 </h2>
-                <p className="mb-8 lg:mb-16 font-light text-center text-gray-400 sm:text-xl">
+                <p className="mb-8 lg:mb-16 font-light text-center text-text sm:text-xl">
                     Alternatively, send us an email mail@journals.gg
                 </p>
                 <form onSubmit={onSubmit} className="space-y-8">
                     <div>
                         <label
                             htmlFor="email"
-                            className="block mb-2 text-sm font-medium text-gray-300"
+                            className="block mb-2 text-sm font-medium text-text"
                         >
                             Your email
                         </label>
@@ -54,14 +54,14 @@ function ContactForm() {
                             type="email"
                             name="email"
                             required
-                            className="border text-sm rounded-lg focus:ring-primary-500 focus:border-primary-500 block w-full p-2.5 bg-gray-700 border-gray-600 placeholder-gray-400 text-white shadow-sm-light"
+                            className="border text-sm rounded-lg focus:ring-primary-500 focus:border-primary-500 block w-full p-2.5 bg-background border-gray-600 placeholder-gray-400 text-text shadow-sm-light"
                             placeholder="billgates@microsoft.com"
                         />
                     </div>
                     <div>
                         <label
                             htmlFor="subject"
-                            className="block mb-2 text-sm font-medium text-gray-300"
+                            className="block mb-2 text-sm font-medium text-text"
                         >
                             Subject
                         </label>
@@ -69,14 +69,14 @@ function ContactForm() {
                             type="text"
                             name="name"
                             required
-                            className="block p-3 w-full text-sm rounded-lg border focus:ring-primary-500 focus:border-primary-500 bg-gray-700 border-gray-600 placeholder-gray-400 text-white shadow-sm-light"
+                            className="block p-3 w-full text-sm rounded-lg border focus:ring-primary-500 focus:border-primary-500 bg-background border-gray-600 placeholder-gray-400 text-text shadow-sm-light"
                             placeholder="Let us know how we can help you"
                         />
                     </div>
                     <div className="sm:col-span-2">
                         <label
                             htmlFor="message"
-                            className="block mb-2 text-sm font-medium text-gray-400"
+                            className="block mb-2 text-sm font-medium text-text"
                         >
                             Your message
                         </label>
@@ -84,11 +84,11 @@ function ContactForm() {
                             name="message"
                             rows={6}
                             required
-                            className="block p-2.5 w-full text-sm rounded-lg border border-gray-600 focus:ring-primary-500 focus:border-primary-500 bg-gray-700 placeholder-gray-400 text-white shadow-sm-light"
+                            className="block p-2.5 w-full text-sm rounded-lg border border-gray-600 focus:ring-primary-500 focus:border-primary-500 bg-background placeholder-gray-400 text-text shadow-sm-light"
                             placeholder="Your message"
                         ></textarea>
                     </div>
-                    <button type="submit" className="w-full text-white bg-primary-600 hover:bg-primary-700 focus:ring-4 focus:outline-none focus:ring-primary-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center bg-gray-700 border-gray-600 placeholder-gray-400 shadow-sm-light">
+                    <button type="submit" className="w-full text-white bg-highlight hover:bg-primary-700 focus:ring-4 focus:outline-none focus:ring-primary-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center bg-gray-700 border-gray-600 placeholder-gray-400 shadow-sm-light">
                         Send message
                     </button>
                     <span className="block mt-4 text-center text-white">{result}</span>
