133f5446f3aaeb00e0f2319d40002d461e2f4c2f
Edward Thomas <90358655+edwardfgt@users.noreply.github.com>
2024-09-20 15:43:27 +0100

    Add styling and a div to Homepage component

--- src/components/Homepage.tsx
+++ src/components/Homepage.tsx
@@ -1,5 +1,9 @@
 function Home() {
-    return <h1 className="text-2xl font-bold underline">Welcome to the Homepage</h1>;
+    return (
+        <div className="bg-background min-h-screen flex items-center justify-center">
+            <h1 className="text-2xl font-bold underline">Welcome to the Homepage</h1>
+        </div>
+    )
 }
 
 export default Home;
