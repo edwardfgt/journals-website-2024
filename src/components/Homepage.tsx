dcfc7ccb0d32716adddc4bdbc4e303f45019b2a6
Edward Thomas <90358655+edwardfgt@users.noreply.github.com>
2024-09-23 16:45:50 +0100

    make HeadingBlock full screen on Homepage

--- src/components/Homepage.tsx
+++ src/components/Homepage.tsx
@@ -4,11 +4,14 @@ import IndustryBlock from './IndustryBlock';
 
 const Home: React.FC = () => {
     return (
-        <div className="bg-background min-h-screen flex flex-col items-center justify-center">
-            <HeadingBlock />
-            <StatBar />
-            <p className='text-text pt-5 text-lg'>Creator? <a href="https://creator.journals.gg/" className="text-highlight">Click here</a></p>
-            <div className="flex flex-col items-center justify-center mt-40">
+        <div className="bg-background min-h-screen flex flex-col items-center">
+            <div className="flex flex-col items-center justify-center min-h-screen">
+                <HeadingBlock />
+                <StatBar />
+                <p className='text-text pt-5 text-lg'>Creator? <a href="https://creator.journals.gg/" className="text-highlight">Click here</a></p>
+            </div>
+
+            <div className="flex flex-col items-center justify-center">
                 <h1 className="text-6xl font-bold text-text">
                     Our Industries
                 </h1>
