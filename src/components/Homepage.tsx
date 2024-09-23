9c08526c382810c0facad8df1215c3a6cc264c79
Edward Thomas <90358655+edwardfgt@users.noreply.github.com>
2024-09-23 16:01:00 +0100

    Add an Industries div and H1

--- src/components/Homepage.tsx
+++ src/components/Homepage.tsx
@@ -6,6 +6,11 @@ function Home() {
         <div className="bg-background min-h-screen flex flex-col items-center justify-center">
             <HeadingBlock />
             <StatBar />
+            <div className="flex flex-col items-center justify-center mt-40">
+                <h1 className="text-6xl font-bold text-text">
+                    Our Industries
+                </h1>
+            </div>
         </div>
     )
 }
