5aeca6b5aa0f3ead7330f415b063aa30950fbc72
Edward Thomas <90358655+edwardfgt@users.noreply.github.com>
2024-09-24 17:09:59 +0100

    Change div to flex-row and stack Industry Blocks Horizontally

--- src/components/Homepage.tsx
+++ src/components/Homepage.tsx
@@ -4,30 +4,40 @@ import IndustryBlock from './IndustryBlock';
 
 const Home: React.FC = () => {
     return (
-        <div className="bg-background min-h-screen flex flex-col items-center">
-            <div className="flex flex-col items-center justify-center min-h-screen">
+        <div className="bg-background min-h-screen flex flex-col items-center pb-20">
+            <div className="flex flex-col items-center justify-center min-h-screen pb-20">
                 <HeadingBlock />
                 <StatBar />
                 <p className='text-text pt-5 text-lg'>Creator? <a href="https://creator.journals.gg/" className="text-highlight">Click here</a></p>
             </div>
 
             <div className="flex flex-col items-center justify-center">
-                <h1 className="text-6xl font-bold text-text">
+                <h1 className="text-4xl font-bold text-text">
                     Our Industries
                 </h1>
             </div>
-            <div className="flex flex-col items-center justify-center mt-10 w-full">
+
+            <div className="flex flex-row items-start justify-center mt-10 w-full space-x-6">
                 <IndustryBlock
                     name="Finance"
                     description="Innovative solutions and cutting-edge technology."
                     audienceStats="60K+ readers"
                     companyNames={["TechCorp", "InnovateX", "FutureTech"]}
+                    imageUrl="https://www.geographicguide.com/united-states/nyc/antique/wall-street/images/vintage-images-ny.jpg"
                 />
                 <IndustryBlock
-                    name="Healthcare"
+                    name="Programming"
                     description="Advancing medical science and patient care."
                     audienceStats="500K+ readers"
                     companyNames={["HealthPlus", "MediCare", "Wellness Inc."]}
+                    imageUrl="https://images.pond5.com/1980s-early-computer-geek-checks-footage-090681496_iconl.jpeg"
+                />
+                <IndustryBlock
+                    name="Gaming"
+                    description="Advancing medical science and patient care."
+                    audienceStats="500K+ readers"
+                    companyNames={["HealthPlus", "MediCare", "Wellness Inc."]}
+                    imageUrl="https://preview.redd.it/the-1980s-when-you-went-to-the-games-instead-of-the-games-v0-750wf32iny7b1.png?width=1080&crop=smart&auto=webp&s=b4b5816b7c9d17f26f7f2ab6a9bd80912b8d6ce4"
                 />
             </div>
         </div>
