09380044d439e9b47de0cc5e76adc4f7844b67e0
Edward Thomas <90358655+edwardfgt@users.noreply.github.com>
2024-09-24 17:09:30 +0100

    Restyle IndustryBlock.tsx component to use Daisy.ui

--- src/components/IndustryBlock.tsx
+++ src/components/IndustryBlock.tsx
@@ -3,20 +3,30 @@ interface IndustryBlockProps {
     description: string;
     audienceStats: string;
     companyNames: string[];
+    imageUrl: string;
 }
 
-const IndustryBlock: React.FC<IndustryBlockProps> = ({ name, description, audienceStats, companyNames }) => {
+const IndustryBlock: React.FC<IndustryBlockProps> = ({ name, description, audienceStats, companyNames, imageUrl }) => {
     return (
-        <div className="p-4 mb-4 w-3/5 flex flex-col items-center">
-            <h2 className="text-4xl font-bold mb-2 text-text text-center">{name}</h2>
-            <p className="text-lg mb-2 text-text text-center">{description}</p>
-            <p className="text-md mb-2 text-text text-center">Audience: {audienceStats}</p>
-            <h3 className="text-xl font-semibold mb-2 text-text text-center">Companies:</h3>
-            <ul className="list-disc list-inside text-text text-center">
-                {companyNames.map((company, index) => (
-                    <li key={index}>{company}</li>
-                ))}
-            </ul>
+        <div className="card bg-text w-96 shadow-xl rounded-b-lg">
+            <figure>
+                <img
+                    src={imageUrl}
+                    alt={name}
+                    className="h-48 w-full object-cover rounded-t-lg"
+                />
+            </figure>
+            <div className="card-body bg-text rounded-b-lg">
+                <h2 className="card-title text-white text-4xl">{name}</h2>
+                <p className="text-white">{description}</p>
+                <p className="text-white">Audience: {audienceStats}</p>
+                <h3 className="text-xl font-semibold mb-2 text-white">Companies:</h3>
+                <ul className="list-disc list-inside text-white">
+                    {companyNames.map((company, index) => (
+                        <li key={index}>{company}</li>
+                    ))}
+                </ul>
+            </div>
         </div>
     )
 }
