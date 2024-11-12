2cf7cb32dac07c3387a6b3f34a1ad7155e34b6b0
Edward Thomas <90358655+edwardfgt@users.noreply.github.com>
2024-11-12 11:37:05 +0000

    Update IndustryBlock.tsx to remove old props and change description prop type

--- src/components/IndustryBlock.tsx
+++ src/components/IndustryBlock.tsx
@@ -1,12 +1,10 @@
 interface IndustryBlockProps {
     name: string;
-    description: string;
-    audienceStats: string;
-    companyNames: string[];
+    description: React.ReactNode;
     imageUrl: string;
 }
 
-const IndustryBlock: React.FC<IndustryBlockProps> = ({ name, description, audienceStats, companyNames, imageUrl }) => {
+const IndustryBlock: React.FC<IndustryBlockProps> = ({ name, description, imageUrl }) => {
     return (
         <div className="card bg-text w-96 shadow-xl rounded-b-lg">
             <figure>
@@ -19,12 +17,7 @@ const IndustryBlock: React.FC<IndustryBlockProps> = ({ name, description, audien
             <div className="card-body bg-text rounded-b-lg">
                 <h2 className="card-title text-white text-4xl">{name}</h2>
                 <p className="text-white">{description}</p>
-                <p className="text-white">Audience: {audienceStats}</p>
-                <h3 className="text-xl font-semibold mb-2 text-white">Companies:</h3>
                 <ul className="list-disc list-inside text-white">
-                    {companyNames.map((company, index) => (
-                        <li key={index}>{company}</li>
-                    ))}
                 </ul>
             </div>
         </div>
