274fee1616f2cc2222eeb7a16c577fceb7341957
Edward Thomas <90358655+edwardfgt@users.noreply.github.com>
2024-11-12 16:25:09 +0000

    Change css in Navbar to align text on the y axis

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -5,8 +5,8 @@ const Navbar: React.FC = () => {
     return (
         <nav className="bg-background p-4">
             <div className="container mx-auto flex items-center">
-                <Link to="/" className="text-text font-bold text-xl pr-8">Journals.gg</Link>
-                <div className="space-x-4">
+                <Link to="/" className="text-text font-bold text-xl">Journals.gg</Link>
+                <div className="space-x-4 flex items-center ml-8">
                     <Link to="/contact" className="btn btn-ghost text-text hover:bg-highlight hover:text-white normal-case">Contact</Link>
                     <Link to="https://creator.journals.gg/" className="btn btn-ghost text-text hover:bg-highlight hover:text-white normal-case">Grow a newsletter</Link>
                 </div>
