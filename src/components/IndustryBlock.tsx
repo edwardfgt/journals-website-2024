c1105d828b9224571b7e73b27be42347e2906a93
Edward Thomas <90358655+edwardfgt@users.noreply.github.com>
2024-11-12 16:00:41 +0000

    Update div styling in IndustryBlock.tsx

--- src/components/IndustryBlock.tsx
+++ src/components/IndustryBlock.tsx
@@ -14,7 +14,7 @@ const IndustryBlock: React.FC<IndustryBlockProps> = ({ name, description, imageU
                     className="h-48 w-full object-cover rounded-t-lg"
                 />
             </figure>
-            <div className="card-body bg-text rounded-b-lg">
+            <div className="card-body bg-text rounded-b-lg industry-block-height">
                 <h2 className="card-title text-white text-4xl">{name}</h2>
                 <p className="text-white">{description}</p>
                 <ul className="list-disc list-inside text-white">
