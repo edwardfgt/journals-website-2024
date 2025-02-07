192164df4064046fb8f2e2ec5752269c89f507fe
Edward Thomas <90358655+edwardfgt@users.noreply.github.com>
2025-02-07 16:40:31 +0000

    Remove emails sent stat

--- src/components/StatBar.tsx
+++ src/components/StatBar.tsx
@@ -3,10 +3,6 @@ import React from 'react';
 const StatBar: React.FC = () => {
     return (
         <div className="stats shadow bg-highlight flex-wrap">
-            <div className="stat min-w-[200px]">
-                <div className="stat-title text-text">Emails Sent</div>
-                <div className="stat-value text-text text-2xl sm:text-4xl">12M+</div>
-            </div>
 
             <div className="stat min-w-[200px]">
                 <div className="stat-title text-text">Publications</div>
